c056a36c69c2429e858ca8f3300bba65a1d7936e
hurtovaanastasia <hurtovanasta85@gmail.com>
2026-01-16 22:56:31 +0300

    заменила объявление главного класса на стандартное

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
-void main() {
-System.out.println("Как тебя зовут?");
-Scanner in = new Scanner(System.in);
-String name = in.nextLine();
-System.out.println("Привет, " + name + "!");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Как тебя зовут?");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("Привет, " + name + "!");
+    }
 }
